5a46585cbba698369c81be9edc6206911ae607f0
SethStephens32 <sethstephens2018@gmail.com>
2023-01-14 10:24:32 -0600

    set up most of the logic for the form fields

--- crown-clothing/src/components/sign-up-form/sign-up-form.component.jsx
+++ crown-clothing/src/components/sign-up-form/sign-up-form.component.jsx
@@ -10,24 +10,34 @@ const defaultFormFields = {
 const SignUpForm = () => {
 
     const [formFields, setFormFields] = useState(defaultFormFields)    
+    const {displayName, email, password, confirmPassword} = formFields
+
+    console.log(formFields)
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setFormFields({...formFields, [name]: value})
+    }
+
+
 
     return (
         <div className="sign-up">
             <h1>Sign up with your email and password</h1>
             <form className="sign-up-form" onSubmit={() => {}}>
                 <label>Display Name</label>
-                <input required type="text"></input>
+                <input required type="text" onChange={handleChange} name="displayName" value={displayName}></input>
 
                 <label>Email</label>
-                <input required type="email"></input>
+                <input required type="email" onChange={handleChange} name="email" value={email}></input>
 
                 <label>Password</label>
-                <input required type="password"></input>
+                <input required type="password" onChange={handleChange} name="password" value={password}></input>
 
                 <label>Confirm Password</label>
-                <input required type="password"></input>
+                <input required type="password" onChange={handleChange} name="confirmPassword" value={confirmPassword}></input>
 
-                <button type="submit">Sign Up</button>
+                <button type="submit" onChange={handleChange}>Sign Up</button>
                 </form>
         </div>
     )
